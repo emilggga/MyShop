d9cf1a2b5a4e72b354a71c71fc42b6b4c8eb88d0
Emil <emilggga@gmail.com>
2026-04-15 16:26:40 +0300

    auto superuser

--- web/apps.py
+++ web/apps.py
@@ -2,4 +2,22 @@ from django.apps import AppConfig
 
 
 class WebConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'web'
+
+    def ready(self):
+        import os
+        from django.contrib.auth import get_user_model
+
+        if os.environ.get("CREATE_SUPERUSER") == "True":
+            User = get_user_model()
+            username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+            password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+            email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+
+            if username and password and not User.objects.filter(username=username).exists():
+                User.objects.create_superuser(
+                    username=username,
+                    email=email,
+                    password=password
+                )
